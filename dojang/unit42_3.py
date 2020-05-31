9344710edafca3df70a35205ae09ec9bc543d45c
ssamko <teacher.ssamko@gmail.com>
2020-05-31 13:21:35 +0900

    functools.wraps 사용

--- dojang/unit42_3.py
+++ dojang/unit42_3.py
@@ -1,6 +1,45 @@
 # 매개변수가 있는 데코레이터
+import functools
+
+
+# def is_multiple(x):
+#     def real_decorator(func):
+#         def wrapper(a, b):
+#             r = func(a, b)
+#             if r % x == 0:
+#                 print(f'{func.__name__}의 반환값은 {x}의 배수입니다.')
+#             else:
+#                 print(f'{func.__name__}의 반환값은 {x}의 배수가 아닙니다.')
+#             return r
+#         return wrapper
+#     return real_decorator
+
+
+# @is_multiple(3)
+# def add(a, b):
+#     return a + b
+
+
+# print(add(10, 20))
+# print(add(2, 5))
+
+# 매개변수가 있는 데코레이터 여러 개 지정하기
+
+
+# @is_multiple(3)
+# @is_multiple(7)
+# def add(a, b):
+#     return a + b
+
+
+# add(10, 20)
+
+# 원래 함수 이름 나오게 하기 functools.wrap 사용
+
+
 def is_multiple(x):
     def real_decorator(func):
+        @functools.wraps(func)
         def wrapper(a, b):
             r = func(a, b)
             if r % x == 0:
@@ -13,20 +52,9 @@ def is_multiple(x):
 
 
 @is_multiple(3)
+@is_multiple(7)
 def add(a, b):
     return a + b
 
 
-print(add(10, 20))
-print(add(2, 5))
-
-# # 매개변수가 있는 데코레이터 여러 개 지정하기
-
-
-# @is_multiple(3)
-# @is_multiple(7)
-# def add(a, b):
-#     return a + b
-
-
-# add(10, 20)
+add(10, 20)
