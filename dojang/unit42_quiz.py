813a94be9bde6544409a92921c4e838ba0352ad9
teacherSsamko <37929557+teacherSsamko@users.noreply.github.com>
2020-06-01 17:44:30 +0900

    정답: isinstance, return

--- dojang/unit42_quiz.py
+++ dojang/unit42_quiz.py
@@ -2,10 +2,12 @@ def type_check(a, b):
     def wrapper(func):
         def inner_wrapper(x, y):
             r = func(x, y)
-            if type(x) == a and type(y) == b:
+            if isinstance(x, a) and isinstance(y, b):
                 return r
             else:
                 raise RuntimeError('자료형이 올바르지 않습니다')
+        return inner_wrapper
+    return wrapper
 
 @type_check(int, int)
 def add(a, b):
