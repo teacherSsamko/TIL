adf414b89354dbb4d1b0cfec4dbcc30f2d20fd1f
ssamko <teacher.ssamko@gmail.com>
2020-06-08 10:31:32 +0900

    fix: overlapped function

--- dojang/unit42_5.py
+++ dojang/unit42_5.py
@@ -9,14 +9,18 @@ class Trace:
 
         return r
 
-@Trace
-def add(a, b):
-    return a + b
+
+# @Trace
+# def add(a, b):
+#     return a + b
+
 
 print(add(10, 20))
 print(add(a=19, b=20))
 
 # 클래스로 매개변수가 있는 데코레이터 만들기
+
+
 class IsMultiple:
     def __init__(self, x):
         self.x = x
@@ -28,13 +32,15 @@ class IsMultiple:
                 print(f'{func.__name__}의 반한값은 {self.x}의 배수입니다.')
             else:
                 print(f'{func.__name__}의 반한값은 {self.x}의 배수가 아닙니다.')
-            
+
             return r
         return wrapper
+
 
 @IsMultiple(3)
 def add(a, b):
     return a + b
+
 
 print(add(10, 20))
 print(add(2, 5))
