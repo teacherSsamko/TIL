aec7ce7127d354226613c6c2710860939ec31b00
ssamko <teacher.ssamko@gmail.com>
2020-06-13 01:01:39 +0900

    unit45.1.3

--- dojang/unit45.module/main.py
+++ dojang/unit45.module/main.py
@@ -3,7 +3,12 @@
 # print(square2.base)
 # print(square2.square(10))
 
-from square2 import base, square
+# from square2 import base, square
 
-print(base)
-print(square(10))
+# print(base)
+# print(square(10))
+
+import person
+
+maria = person.Person('maria', 20, 'huamdong')
+maria.greeting()
