29431603fb0fbb1806feb58853bc09bf14333a87
ssamko <teacher.ssamko@gmail.com>
2020-06-13 00:58:59 +0900

    unit45.1.2

--- dojang/unit45.module/main.py
+++ dojang/unit45.module/main.py
@@ -1,4 +1,9 @@
-import square2
+# import square2
 
-print(square2.base)
-print(square2.square(10))
+# print(square2.base)
+# print(square2.square(10))
+
+from square2 import base, square
+
+print(base)
+print(square(10))
