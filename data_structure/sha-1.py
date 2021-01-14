c8b8ddf5fbb0cc3bbad0482befb47b7227c5298f
ssamko <teacher.ssamko@gmail.com>
2021-01-14 21:26:06 +0900

    hash functions

---
+++ data_structure/sha-1.py
@@ -0,0 +1,8 @@
+import hashlib
+
+data = 'test'.encode()  # encode() => string to byte
+hash_object = hashlib.sha1()
+hash_object.update(data)
+hash_object.update(b'test')  # same with above
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
