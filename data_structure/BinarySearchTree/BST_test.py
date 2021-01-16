8a3177187c8494f95c2a06f7546162d8d58c4b3a
ssamko <teacher.ssamko@gmail.com>
2021-01-16 23:12:13 +0900

    test delete method

--- data_structure/BinarySearchTree/BST_test.py
+++ data_structure/BinarySearchTree/BST_test.py
@@ -17,3 +17,22 @@ for x in bst_nums:
     assert bst.search(x) is True
 
 print('search success')
+
+# delete 10 nodes
+delete_nums = set()
+bst_nums = list(bst_nums)
+while len(delete_nums) < 10:
+    delete_nums.add(bst_nums[random.randint(0, 99)])
+
+for del_num in delete_nums:
+    assert bst.delete(del_num) is not False
+
+# research tree
+
+for x in bst_nums:
+    if x not in delete_nums:
+        assert bst.search(x) is True
+    else:
+        assert bst.search(x) is False
+
+print('delete success')
