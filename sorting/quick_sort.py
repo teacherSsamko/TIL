c8b16025e30c07bd212dd22f6be371939841682c
ssamko <teacher.ssamko@gmail.com>
2021-01-25 20:28:26 +0900

    pythonic qucik sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -17,3 +17,16 @@ def quick_sort(data):
 
 sample = random.sample(range(100), 10)
 print('end:', quick_sort(sample))
+
+
+def qsort(data):
+    if len(data) <= 1:
+        return data
+
+    pivot = data[0]
+    left = [item for item in data[1:] if item < pivot]
+    right = [item for item in data[1:] if item >= pivot]
+    return qsort(left) + [pivot] + qsort(right)
+
+
+print(qsort(sample))
