1c559bb89f4e370c77ac44600858730a51ca7bd1
ssamko <teacher.ssamko@gmail.com>
2021-02-01 22:06:21 +0900

    sample

--- docker_study/flask_api/app.py
+++ docker_study/flask_api/app.py
@@ -10,5 +10,19 @@ def hello():
     return 'hello'
 
 
+@app.route('/upscaling')
+def check_upscaling():
+    url = 'https://host/check_upscaling'
+    res = requests.get(url)
+    return f'{res.status_code} - {res.text}'
+
+
+@app.route('/upload')
+def check_upload():
+    url = 'https://host/upload'
+    data = 'file here'
+    res = requests.post(url, data=data)
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
