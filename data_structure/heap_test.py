81e10948a1849b5501a21f5257315301948e2d2e
ssamko <teacher.ssamko@gmail.com>
2021-03-06 17:35:24 +0900

    heap test

--- data_structure/heap_test.py
+++ data_structure/heap_test.py
@@ -1,4 +1,8 @@
+import random
+
 from heap_ import Heap
+
+# TO DO: why duplicated numbers are inserted?
 
 heap = Heap(15)
 heap.insert(10)
@@ -10,4 +14,17 @@ print(heap.heap_array)
 heap.pop()
 print(heap.heap_array)
 heap.pop()
+print(heap.heap_array)
+
+insert_nums = set()
+first = random.randint(1, 100)
+insert_nums.add(first)
+heap = Heap(first)
+while len(insert_nums) < 10:
+    insert_nums.add(random.randint(1, 15))
+
+insert_nums = list(insert_nums)
+while insert_nums:
+    heap.insert(insert_nums.pop())
+
 print(heap.heap_array)
