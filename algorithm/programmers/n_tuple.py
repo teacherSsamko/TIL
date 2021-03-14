2aac7d2b08837d577d683f1fea8fe2b28be7b48b
ssamko <teacher.ssamko@gmail.com>
2021-03-14 14:42:07 +0900

    update: using re, Counter

--- algorithm/programmers/n_tuple.py
+++ algorithm/programmers/n_tuple.py
@@ -1,21 +1,9 @@
-import re
-
-
 def solution(s):
-    def strtodict(s):
-        # set_ = s.strip("\{\}")
-        p = re.compile("{.*?}")
-        set_ = p.findall(s)
-        set_ = list(map(lambda x: x.strip("\{\}").split(","), set_))
-        return set_
-    answer = []
-    set_ = strtodict(s)
-    set_ = sorted(set_, key=len)
-    for x in set_:
-        new = set(x) - set(answer)
-        answer.append(list(new).pop())
-    answer = list(map(lambda x: int(x), answer))
-    return answer
+    import re
+    from collections import Counter
+
+    c = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(c.items(), key=lambda x: x[1], reverse=True)]))
 
 
 s = input()
