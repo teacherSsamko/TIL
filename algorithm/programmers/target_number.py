8aa58bf63a4b2177659eb4b84e3df5a64e335f30
ssamko <teacher.ssamko@gmail.com>
2021-03-17 22:39:12 +0900

    myself

--- algorithm/programmers/target_number.py
+++ algorithm/programmers/target_number.py
@@ -6,19 +6,16 @@ def dfs(array, numbers, target, size):
         else:
             return 0
     else:
-        A = numbers.pop(0)
-        for i in [1, -1]:
-            array.append(A * i)
-            answer += dfs(array, numbers, target, size)
-            array.pop()
-        numbers.append(A)
-        return answer
+        answer += dfs(array + [numbers[0]], numbers[1:], target, size)
+        answer += dfs(array + [-numbers[0]], numbers[1:], target, size)
+    return answer
 
 
 def solution(numbers, target):
     answer = 0
     answer += dfs([numbers[0]], numbers[1:], target, len(numbers))
     answer += dfs([-numbers[0]], numbers[1:], target, len(numbers))
+
     return answer
 
 
