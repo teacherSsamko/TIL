6df40432a92875ddbe141e58812944f3a7929a09
ssamko <teacher.ssamko@gmail.com>
2021-03-19 21:00:34 +0900

    update

--- boj/boj1920.py
+++ boj/boj1920.py
@@ -1,9 +1,17 @@
-N = int(input())
-A = set(input().split())
-M, B = int(input()), input().split()
+def solution(N, M):
+    N = {k: 1 for k in N}
+    for m in M:
+        if N.get(m):
+            print(1)
+        else:
+            print(0)
 
-for i in B:
-    if i in A:
-        print(1)
-    else:
-        print(0)
+    return
+
+
+N = int(input())
+Ns = list(map(int, input().split()))
+M = int(input())
+Ms = list(map(int, input().split()))
+
+solution(Ns, Ms)
