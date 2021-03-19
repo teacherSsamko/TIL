6c4e741fdbe53e0f34365a891919cfe50b49174f
ssamko <teacher.ssamko@gmail.com>
2021-03-19 20:40:01 +0900

    using stack

--- boj/boj5397.py
+++ boj/boj5397.py
@@ -1,22 +1,21 @@
 def solution(logs):
     cursor = 0
     origin = []
+    buffer = []
     for log in logs:
         if log == "<":
-            if cursor > 0:
-                cursor -= 1
+            if origin:
+                buffer.append(origin.pop())
         elif log == ">":
-            if cursor < len(origin):
-                cursor += 1
+            if buffer:
+                origin.append(buffer.pop())
         elif log == "-":
-            if origin and cursor > 0:
-                del origin[cursor - 1]
-                cursor -= 1
+            if origin:
+                origin.pop()
         else:
-            origin.insert(cursor, log)
-            cursor += 1
+            origin.append(log)
 
-    return "".join(origin)
+    return "".join(origin + buffer[::-1])
 
 
 N = int(input())
