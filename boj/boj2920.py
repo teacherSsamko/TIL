0dc79694ec34e7ee78d5849fb10277be0fb1d63a
ssamko <teacher.ssamko@gmail.com>
2021-03-19 14:06:56 +0900

    shorter

--- boj/boj2920.py
+++ boj/boj2920.py
@@ -1,26 +1,22 @@
 scores = list(map(int, input().split()))
-asc = None
-start = scores.pop(0)
-for score in scores:
-    if start < score:
-        if asc is None:
-            asc = "ascending"
-            start = score
-        elif asc == "ascending":
-            start = score
-            continue
-        else:
-            asc = "mixed"
-            break
-    else:
-        if asc is None:
-            asc = "descending"
-            start = score
-        elif asc == "descending":
-            start = score
-            continue
-        else:
-            asc = "mixed"
-            break
 
-print(asc)
+
+def solution(scores):
+    asc = 0
+    desc = 0
+    now = scores.pop(0)
+    for nxt in scores:
+        if nxt - now > 0:
+            asc += 1
+        else:
+            desc += 1
+        now = nxt
+    if asc and desc:
+        return "mixed"
+    if asc:
+        return "ascending"
+    else:
+        return "descending"
+
+
+print(solution(scores))
