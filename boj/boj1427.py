fce110511af2bdfcfc24b95e6385f82b66626fca
ssamko <teacher.ssamko@gmail.com>
2021-03-20 09:12:15 +0900

    another solution

--- boj/boj1427.py
+++ boj/boj1427.py
@@ -1,6 +1,11 @@
 N = input()
 n_l = list(map(int, list(N)))
-n_l.sort(reverse=True)
+# n_l.sort(reverse=True)
 
-n_l = list(map(str, n_l))
-print("".join(n_l))
+# n_l = list(map(str, n_l))
+# print("".join(n_l))
+
+for i in range(9, -1, -1):
+    for j in n_l:
+        if i == int(j):
+            print(j, end='')
