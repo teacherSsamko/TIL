93c61f4387696a0b80b23399b5d6cb069988ea43
ssamko <teacher.ssamko@gmail.com>
2021-03-21 21:39:55 +0900

    basic sort

--- boj/boj11004.py
+++ boj/boj11004.py
@@ -1,16 +1,4 @@
-import heapq
-
-
-def solution(nums, K):
-    heapq.heapify(nums)
-    result = None
-    for _ in range(K):
-        result = heapq.heappop(nums)
-        # print(result)
-
-    return result
-
-
 N, K = map(int, input().split())
 nums = list(map(int, input().split()))
-print(solution(nums, K))
+nums.sort()
+print(nums[K - 1])
