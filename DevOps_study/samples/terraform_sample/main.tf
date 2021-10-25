3c9828bf72297e49dff188dbe15181ce080da0db
teacherSsamko <ssamko@kakao.com>
2021-10-25 09:25:59 +0900

    terraform: add variable & use ubuntu AMI

--- DevOps_study/samples/terraform_sample/main.tf
+++ DevOps_study/samples/terraform_sample/main.tf
@@ -9,16 +9,20 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+variable "ec2_name" {
+  type = string
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-03d5c68bab01f3496" // ubuntu 20.04 LTS
   instance_type = "t2.micro"
 
   tags = {
-    Name = "terraformStudy"
+    Name = var.ec2_name
   }
 }
