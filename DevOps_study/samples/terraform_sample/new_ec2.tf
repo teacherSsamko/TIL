acffd3418891044ba532d1cd0cd95f4aa87e7e7e
ssamko <teacher.ssamko@gmail.com>
2021-11-11 18:21:02 +0900

    rmv: old unused codes

--- DevOps_study/samples/terraform_sample/new_ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-03d5c68bab01f3496" // ubuntu 20.04 LTS
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.ec2_name
-  }
-}
--- DevOps_study/samples/terraform_sample/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ec2_name" {
-  type = string
-}
-
-variable "username" {
-    type = list
-    default = ["es-a", "es-b", "es-c"]
-}
