b0686bed3a8eebbbfa910a2ff06569e7e8c41111
ssamko <teacher.ssamko@gmail.com>
2021-11-12 00:43:00 +0900

    tr: ec2: ami variable

--- DevOps_study/terraform_study/new_ec2/new_ec2.tf
+++ DevOps_study/terraform_study/new_ec2/new_ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-03d5c68bab01f3496" // ubuntu 20.04 LTS
-  instance_type = "t2.micro"
+  ami           = var.ami_amazon_linux2
+  instance_type = "t2.small"
 
   tags = {
     Name = var.ec2_name
