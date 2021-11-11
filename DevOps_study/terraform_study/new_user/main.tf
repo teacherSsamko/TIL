cad00a032b151ba96ee1a1dfb688be0b454950a0
ssamko <teacher.ssamko@gmail.com>
2021-11-11 18:19:16 +0900

    tr: main: update version and profile

--- DevOps_study/terraform_study/new_user/main.tf
+++ DevOps_study/terraform_study/new_user/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = ">= 3.63.0"
     }
   }
 
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
-  profile = "hits"
+  profile = "ssamko"
   region  = "us-west-2"
 }
 
