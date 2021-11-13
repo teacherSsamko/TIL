8d46a9649b1e37ecff7f2415374377cb4b8046b1
teacherSsamko <ssamko@kakao.com>
2021-11-13 12:49:22 +0900

    tr: user: policy for instance connect

--- DevOps_study/terraform_study/new_user/new_user.tf
+++ DevOps_study/terraform_study/new_user/new_user.tf
@@ -5,12 +5,16 @@ resource "aws_iam_user" "dev" {
 }
 
 resource "aws_iam_access_key" "newemp" {
-  for_each = toset(var.dev_team)
+  for_each = aws_iam_user.dev
+  user     = each.key
+}
+
+resource "aws_iam_user_policy" "ec2_instance_connect" {
+  for_each = aws_iam_user.dev
+  name     = "ec2_instance_connect"
   user     = each.key
 
-  depends_on = [
-    aws_iam_user.dev
-  ]
+  policy = data.aws_iam_policy_document.ec2_instance_connect[each.key].json
 }
 
 resource "aws_iam_user_group_membership" "developer" {
