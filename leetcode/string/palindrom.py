f4776a841e2066895b6f5bc8a9208d54433d1dab
teacherSsamko <ssamko@kakao.com>
2022-01-29 16:52:33 +0900

    leetcode: fix palindrome

--- leetcode/string/palindrom.py
+++ leetcode/string/palindrom.py
@@ -2,13 +2,8 @@ def isPalindrome(x: int) -> bool:
     if x < 0:
         return False
     x = str(x)
-    mid = len(x) // 2
-    if len(x) % 2:
-        if x[: mid + 1] == x[mid:][::-1]:
-            return True
-    else:
-        if x[:mid] == x[mid:][::-1]:
-            return True
+    if x == x[::-1]:
+        return True
     return False
 
 
