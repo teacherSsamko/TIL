5dc9ab37568fae00ce04f5c39b1ec13f09514fdd
teacherSsamko <ssamko@kakao.com>
2022-03-22 17:52:12 +0900

    leetcode: kadane's algorithm

--- leetcode/max_subarray.py
+++ leetcode/max_subarray.py
@@ -5,11 +5,10 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = -math.inf
-        for i in range(len(nums)):
-            current_sum = 0
-            for j in range(i, len(nums)):
-                current_sum += nums[j]
-                max_sum = max(max_sum, current_sum)
+        cur_sum = max_sum = nums[0]
+
+        for num in nums[1:]:
+            cur_sum = max(num, cur_sum + num)
+            max_sum = max(cur_sum, max_sum)
 
         return max_sum
