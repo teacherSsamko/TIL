4ee1bb368f74ee66314f1e45efbb62ca1fcdb0b3
ssamko <teacher.ssamko@gmail.com>
2022-03-22 12:26:32 +0900

    leetcode: sort: improve reorderLogFiles

--- leetcode/sort/reorderLogFiles.py
+++ leetcode/sort/reorderLogFiles.py
@@ -4,21 +4,8 @@ from typing import List
 
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        # divide logs into two groups of letter and digit
-        letters = []
-        digits = []
-        for log in logs:
-            _id, rest = log.split(" ", maxsplit=1)
-            if rest[0].isdigit():
-                digits.append(log)
-            else:
-                letters.append(log)
-
-        # order letters
         def get_key(log):
             _id, rest = log.split(" ", maxsplit=1)
-            return rest, _id
-        letters.sort(key=get_key)
+            return (0, rest, _id) if rest[0].isalpha() else (1, )
 
-        # merge two groups
-        return letters + digits
+        return sorted(logs, key=get_key)
