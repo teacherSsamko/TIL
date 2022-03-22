cb867c407a40f1e3ad4d2d47dc48e68252c9121c
ssamko <teacher.ssamko@gmail.com>
2022-03-23 00:23:16 +0900

    leetcode: recurssion: symmetricTree

---
+++ leetcode/recursion/symmetricTree.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/symmetric-tree/
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def is_mirror(t1, t2):
+            if t1 is None and t2 is None:
+                return True
+            if t1 is None or t2 is None:
+                return False
+            return (t1.val == t2.val) and is_mirror(t1.right, t2.left) and is_mirror(t1.left, t2.right)
+
+        return is_mirror(root, root)
