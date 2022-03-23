023219fc26e397d0009ac9ed946ccd711793862e
teacherSsamko <ssamko@kakao.com>
2022-03-23 20:59:34 +0900

    leetcode: recursion: maximumDepth

---
+++ leetcode/recursion/maximumDepth.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+
+            return max(left_height, right_height) + 1
