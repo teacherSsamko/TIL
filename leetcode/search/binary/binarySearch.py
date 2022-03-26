12894d96e128905580353ea5797bcdb5e7544bcf
ssamko <teacher.ssamko@gmail.com>
2022-03-26 11:16:46 +0900

    leetcode: binarySearch: using Pointer

--- leetcode/search/binary/binarySearch.py
+++ leetcode/search/binary/binarySearch.py
@@ -4,22 +4,13 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def binarySearch(nums, target, left, right):
-            if left < right and (right - left) > 1:
-                pivot = (left + right) // 2
-                if target == nums[pivot]:
-                    return pivot
-                elif target > nums[pivot]:
-                    # find right
-                    return binarySearch(nums, target, pivot, right)
-                else:
-                    return binarySearch(nums, target, left, pivot)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if nums[pivot] == target:
+                return pivot
+            if target < nums[pivot]:
+                right = pivot - 1
             else:
-                if target == nums[left]:
-                    return left
-                elif target == nums[right]:
-                    return right
-                else:
-                    return -1
-
-        return binarySearch(nums, target, 0, len(nums) - 1)
+                left = pivot + 1
+        return -1
