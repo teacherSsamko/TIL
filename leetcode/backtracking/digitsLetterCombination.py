ef062e7f4a5b34448a421c9994d4b28d9db178fc
teacherSsamko <ssamko@kakao.com>
2022-03-26 13:34:23 +0900

    leetcode: backtrack: digitsLetterCombinations

---
+++ leetcode/backtracking/digitsLetterCombination.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        map_ = {
+            2: ["a", "b", "c"],
+            3: ["d", "e", "f"],
+            4: ["g", "h", "i"],
+            5: ["j", "k", "l"],
+            6: ["m", "n", "o"],
+            7: ["p", "q", "r", "s"],
+            8: ["t", "u", "v"],
+            9: ["w", "x", "y", "z"],
+        }
+
+        ans = []
+
+        def backtrack(index, path):
+            nonlocal ans
+            nonlocal digits
+            if len(path) == len(digits):
+                ans.append("".join(path))
+                return
+
+            possible_letters = map_[int(digits[index])]
+            for letter in possible_letters:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return ans
