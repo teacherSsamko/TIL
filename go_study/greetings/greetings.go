8ab78a4a7599fed462fc9ecf4dc6d97b84419aff
teacherSsamko <ssamko@kakao.com>
2022-04-14 10:46:02 +0900

    go: modulize and replace import path

--- go_study/greetings/greetings.go
+++ go_study/greetings/greetings.go
@@ -1,16 +1,8 @@
 package greetings
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-// Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
-	if name == "" {
-		return "", errors.New("empty name")
-	}
+func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
+	return message
 }
